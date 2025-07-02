1cb8d2c8e7918166b24e8762ee6eff5b013ab42e
KApollinaria <korovkina39@yandex.ru>
2025-07-02 13:59:32 +0500

    Update lesson_3_task_3.py

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,15 +1,14 @@
-from Address import address
-from Mailing import mailing
+from address import Address
+from mailing import Mailing
 
 
 # Создаем объекты класса Address
 to_address = Address("1111", "Кунгур", "Центральная", "1", "4")
 from_address = Address("2222", "Пермь", "Ленина", "2", "3")
-track = Address("3333", "Москва", "Набережая", "3", "2")
-cost = Address("4444", "Уфа", "Падерная", "4", "1")
+
 
 # Создаем объект класса Mailing
-Mailing = Mailing(to_address, from_address, track, cost)
+Mailing = Mailing(to_address, from_address, 333, "TRK666666")
 
 # Выводим информацию
 print(Mailing)
