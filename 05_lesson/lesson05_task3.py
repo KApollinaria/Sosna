3c4272d9529dc8b27bdc211af79edbdd2c457680
KApollinaria <korovkina39@yandex.ru>
2025-07-22 21:57:26 +0500

    5

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from seleniun.webdriver.common.by import By
+from webdriver.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
+
+
+driver.get('http://the-internet.herokuapp.com/inputs')
+
+input_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_elememt_locater((By.TAG_NAME, "input"))
+)
+
+input_field.send_keys("Sky")
+
+input_field.clear()
+
+input_field.send_keys("Pro")
+
+
+driver.quit()
