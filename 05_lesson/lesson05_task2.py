562a5b29158032c95b3f32b92710409beea72ca4
KApollinaria <korovkina39@yandex.ru>
2025-07-23 14:49:19 +0500

    сохранение изменений

--- 05_lesson/lesson05_task2.py
+++ 05_lesson/lesson05_task2.py
@@ -1,8 +1,8 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from seleniun.webdriver.chrome.by import By
+from selenium.webdriver.common.service import Service as ChromeService
+from webdriver_manager.common import ChromeDriverManager
+from seleniun.webdriver.common.by import By
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
@@ -10,11 +10,10 @@ driver = webdriver.Chrome(
 
 driver.get('http://uitestingplayground.com/dynamicid')
 
-for i in range(3):
-    driver.find_element(By.CSS_SELECTOR, 'button.btn-primary')
-    driver.click()
-    alart = driver.switch_to.alert
-    alart.accept()
-    driver.refresh
 
-sleep(50)
+button = driver.find_element(By.CSS_SELECTOR, 'button.btn-primary')
+sleep(2)
+button.click()
+
+
+driver.quit()
--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.service import Service as FirefoxService
+from webdriver_manager.common import GeckoDriverManager
 
-driver = webdriver.Firefox()
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
+
+
 driver.get("https://the-internet.herokuapp.com/login")
 
 username_input = driver.find_element(By.XPATH, '//input[@id="username"]')
@@ -13,3 +19,6 @@ password_input.send_keys("SuperSecretPassword!", Keys.RETURN)
 
 button = driver.find_element(By.CSS_SELECTOR, "button.radius")
 button.click()
+
+
+driver.quit()
