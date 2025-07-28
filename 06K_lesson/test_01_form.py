c90bcee380755f1e79d9045d0e34645e1fd947c9
KApollinaria <korovkina39@yandex.ru>
2025-07-28 22:34:10 +0500

    сохранение изменений

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -8,14 +8,16 @@ def test_form():
     driver.get(
         "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
+    driver.maximize_window()
+
     # Заполнение формы
     driver.find_element(By.NAME, "first-name").send_keys("Иван")
     driver.find_element(By.NAME, "last-name").send_keys("Петров")
     driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
-    driver.find_element(By.NAME, "city").send_keys("Москва")
-    driver.find_element(By.NAME, "country").send_keys("Россия")
     driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
     driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
     driver.find_element(By.NAME, "job-position").send_keys("QA")
     driver.find_element(By.NAME, "company").send_keys("SkyPro")
 
@@ -27,7 +29,7 @@ def test_form():
     assert pole_z == "alert py-2 alert-danger"
 
     # Проверка подсветки остальных полей
-    poles = ["firstName", "lastName", "address", "email", "phone", "city", "country", "job", "company"]
+    poles = ["#first-name", "#last-name", "#address", "#e-mail", "#phone", "#city", "#country", "#job-position", "#company"]
     for pole in poles:
         pole_class = driver.find_element(By.CSS_SELECTOR, pole).get_attribute("class")
         assert pole_class == "alert py-2 alert-success"
